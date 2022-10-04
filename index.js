4b8b5215faf947ab518a5dddbe3f1edae93bf6d9
alindaByamukama <alindabyamukama@gmail.com>
2022-10-04 10:20:56 +0300

    Adds day 2 classwork - nodejs --> express

---
+++ index.js
@@ -0,0 +1,5 @@
+// import the dependancy express
+const express = require('express')
+// import express into the server file - entry point of our project
+// this means we are using express as our server
+const app = express()
