8ce029810ac39ac2646803c2a387a1db0cf4d47a
alindaByamukama <alindabyamukama@gmail.com>
2022-10-05 10:14:17 +0300

    Adds classword from day three --> structure of an express application

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+// dependencies
 // import the dependancy express
 const express = require("express");
 
 // import express into the server file - entry point of our project
 // this means we are using express as our server
+// instantiations
 const app = express();
 
+// configurations
+// middleware
+
+// routes
+// structure of a route --> app.METHOD(PATH, HANDLER)
 // creates a route for the homepage found at '/'
 app.get("/", (req, res) => {
   res.send("Homepage! Hello World.");
@@ -14,6 +21,7 @@ app.get("/about", (req, res) => {
     res.send("About page! Learn more about us.");
   });
 
+// bootstrapping server
 app.listen(3000, () =>
   console.log("Listening on port 3000 ... server complete")
 );
