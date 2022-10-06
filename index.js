f373efe29a130f41551fccbf05bfca02d880b8ae
alindaByamukama <alindabyamukama@gmail.com>
2022-10-06 10:24:26 +0300

    Adds middleware, routes w/ params, put, post, delete methods used in routes - day four testing w/ postman

--- index.js
+++ index.js
@@ -8,7 +8,29 @@ const express = require("express");
 const app = express();
 
 // configurations
+
 // middleware
+// a session is often used in applications suchas login/signup
+// app.use(
+//   session({
+//     secret: 'arbitary-string',
+//     resave: false,
+//     saveUninitialized: true,
+//     cookie: { secure: true }
+//   })
+// );
+// process incoming data such as body payload - data received from client
+app.use(express.urlencoded({ extended: false }));
+// a middleware function
+// Simple request time logger
+app.use((req, res, next) => {
+  console.log("A new request received at " + Date.now());
+
+  // This function call tells that more processing is
+  // required for the current request and is in the next middleware
+  // function/route handler.
+  next();
+});
 
 // routes
 // structure of a route --> app.METHOD(PATH, HANDLER)
@@ -17,9 +39,39 @@ app.get("/", (req, res) => {
   res.send("Homepage! Hello World.");
 });
 
+app.post("/register", (req, res) => {
+  res.send("You have registered a user");
+}); 
+  
+app.delete("/about", (req, res) => {
+  res.send("You have deleted something");
+}); 
+
+app.put("/about", (req, res) => {
+  res.send("I have been changed!");
+});
+
 app.get("/about", (req, res) => {
-    res.send("About page! Learn more about us.");
-  });
+  res.send("About page! Learn more about us.");
+});
+
+//Simple request time logger for a specific route
+// app.use('/about', (req, res, next) => {
+//   console.log('A new request received at ' + Date.now());
+//   next();
+// });
+
+// route params are named URL segments used to capture
+// values specified at their position in the URL
+// req.params object is used to access all params passed in the url
+app.get("/books/:bookId", (req, res) => {
+  res.send(req.params);
+});
+
+// For invalid routes
+app.get("*", (req, res) => {
+  res.send("404! This is an invalid URL.");
+});
 
 // bootstrapping server
 app.listen(3000, () =>
