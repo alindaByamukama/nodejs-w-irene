5ff1bce54e4dbd772e2bf5daaedf1f442e707074
alindaByamukama <alindabyamukama@gmail.com>
2022-10-13 15:00:43 +0300

    Adds schema for our db

---
+++ models/User.js
@@ -0,0 +1,55 @@
+const { text } = require("express");
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  firstname: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  telephone: {
+    type: String,
+    required: true,
+  },
+  dob: {
+    type: Date,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+  nin: {
+    type: String,
+    required: true,
+  },
+  regDate: {
+    type: String,
+    required: true,
+  },
+  mStatus: {
+    type: String,
+    required: true,
+  },
+});
